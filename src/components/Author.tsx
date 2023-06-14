875e7637ebe3c6b99984a3e236bdda31fd6d196f
André Ribas <andre@andev.com.br>
2023-06-13 21:21:07 -0300

    Update Author.tsx

--- src/components/Author.tsx
+++ src/components/Author.tsx
@@ -6,7 +6,7 @@ type Props = {
 export default function Author({ author }: Props) {
   return (
     <>
-      <span>{author.name}</span>
+      <span><a href=https://https://blog.andev.us/about/{author.link}>{author.name}</a></span>
       <style jsx>
         {`
           span {
