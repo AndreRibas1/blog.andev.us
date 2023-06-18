817cc1ccac872e16fa3a543a1bc395a4333e5998
André Ribas <andre@andev.com.br>
2023-06-18 11:53:01 -0300

    Update Layout.tsx
    Signed-off-by: André Ribas <andre@andev.com.br>

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -14,6 +14,27 @@ export default function Layout({ children }: Props) {
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="apple-touch-icon" href="/icon.png" />
         <meta name="theme-color" content="#fff" />
+        <script type="text/javascript">
+//<![CDATA[
+var owa_baseUrl = 'http://analytics.arnext.net/';
+var owa_cmds = owa_cmds || [];
+owa_cmds.push(['setSiteId', 'e3a403b8dd9657d4e40cba9666fb9149']);
+owa_cmds.push(['trackPageView']);
+owa_cmds.push(['trackClicks']);
+owa_cmds.push(['trackDomStream']);
+
+(function() {
+    var _owa = document.createElement('script');
+    _owa.type = 'text/javascript';
+    _owa.async = true;
+    owa_baseUrl = ('https:' == document.location.protocol ? window.owa_baseSecUrl || owa_baseUrl.replace(/http:/, 'https:') : owa_baseUrl);
+    _owa.src = owa_baseUrl + 'modules/base/dist/owa.tracker.js';
+    var _owa_s = document.getElementsByTagName('script')[0];
+    _owa_s.parentNode.insertBefore(_owa, _owa_s);
+})();
+//]]>
+</script>
+
       </Head>
       <nav>
         <Navigation />
