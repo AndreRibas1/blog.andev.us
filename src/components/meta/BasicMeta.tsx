6d9b8b35b7120db1653cc6a30b86c28b41526c2b
André Ribas <andre@andev.com.br>
2023-08-20 11:21:40 -0300

    Update BasicMeta.tsx
    Signed-off-by: André Ribas <andre@andev.com.br>

--- src/components/meta/BasicMeta.tsx
+++ src/components/meta/BasicMeta.tsx
@@ -34,6 +34,8 @@ export default function BasicMeta({
       />
       {author ? <meta name="author" content={author} /> : null}
       <link rel="canonical" href={config.base_url + url} />
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8739497095394372"
+     crossorigin="anonymous"></script>
     </Head>
   );
 }
